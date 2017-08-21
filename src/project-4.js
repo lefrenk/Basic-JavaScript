6d07d8a2cadc29028965442aa16cc655f3e156af
Adam Lower <adamlowerj@gmail.com>
2017-08-21 15:01:57 -0400

    Finished project-4.js

--- src/project-4.js
+++ src/project-4.js
@@ -7,22 +7,26 @@ const getLength = (collection, cb) => cb(collection.length);
 const getLastItem = (collection, cb) => cb(collection[collection.length - 1]);
   // Write a function called getLastItem which passes the getLastItem item of the array into the callback
 
-const sumNums = (x, y, cb) => {
+const sumNums = (x, y, cb) => cb(x + y);
   // Write a function called sumNums that adds two numbers and passes the result to the callback
-};
 
-const multiplyNums = (x, y, cb) => {
+const multiplyNums = (x, y, cb) => cb(x * y);
   // Write a function called multiplyNums that adds two numbers and passes the result to the callback
-};
 
-const contains = (collection, item, cb) => {
+const contains = (collection, item, cb) => cb(collection.indexOf(item) > -1);
   // Write a function called contains that checks if an item is present inside of the given array.
   // Pass true to the callback if it is, otherwise pass false
-};
 
 const removeDuplicates = (collection, cb) => {
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
   // Pass the array to the callback function.  Do not mutate the original array.
+  const noDuplicates = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (!(noDuplicates.includes(collection[i]))) {
+      noDuplicates.push(collection[i]);
+    }
+  }
+  cb(noDuplicates);
 };
 
 module.exports = {
